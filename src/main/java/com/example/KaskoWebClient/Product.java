28d3770f616eaf6c317a56f9c970cfdec8ad0ac4
pavel1944 <pavel280101@mail.ru>
2022-06-29 20:23:20 +0300

    feature-1. Add Product Entity.

---
+++ src/main/java/com/example/KaskoWebClient/Product.java
@@ -0,0 +1,15 @@
+package com.example.KaskoWebClient;
+
+public class Product {
+    private String ID_product;
+    private String product_name;
+
+    public String get_ID_product() { return ID_product; }
+
+    public String get_name() { return product_name; }
+
+    public void setName(String name) { product_name = name; }
+
+    public void setID(String ID) { ID_product = ID; }
+
+}
--- src/main/java/com/example/KaskoWebClient/SampleController.java
+++ src/main/java/com/example/KaskoWebClient/SampleController.java
@@ -19,4 +19,32 @@ public class SampleController {
         model.addAttribute("elements", elements);
         return "element";
     }
+
+    @GetMapping("/products")
+    public String listProduct(Model model) {
+        class Product {
+            private final String ID_product;
+            private final String product_name;
+            public String get_ID_product() { return ID_product; }
+            public String get_name() { return product_name; }
+            public Product (String ID_product, String product_name) {
+                this.ID_product = ID_product;
+                this.product_name = product_name;
+            }
+        }
+
+        Product[] prod = new Product[5];
+
+        prod[0] = new Product("123", "bananas");
+        prod[1] = new Product("234", "watermelon");
+        prod[2] = new Product("577", "grape");
+        prod[3] = new Product("12", "cherry");
+        prod[4] = new Product("56", "blueberry");
+
+        List<String> elements = List.of(prod[0].get_ID_product(), prod[0].get_name(), prod[1].get_ID_product(),
+                prod[1].get_name(), prod[2].get_ID_product(), prod[2].get_name(), prod[3].get_ID_product(),
+                prod[3].get_name(), prod[4].get_ID_product(), prod[4].get_name());
+        model.addAttribute("elements", elements);
+        return "list";
+    }
 }
